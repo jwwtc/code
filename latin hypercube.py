3f89d34cbb5a64de12f50a65f96743bcb6f20e54
Michael <paleosmichael@protonmail.com>
2022-01-29 02:59:43 +0200

    latin hypercube

--- latin hypercube.py
+++ latin hypercube.py
@@ -1,18 +1,27 @@
+#%% packages
+
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 from smt.sampling_methods import LHS
 
-xlimits=np.array([[300, 400], [2, 10]])
+#%% sampling --- Temperature // Duration
+
+xlimits=np.array([[700, 1000], [300, 600]])
 sampling=LHS(xlimits=xlimits)
 
-num = 10
+num = 20
 x = sampling(num)
+
+print(x)
+print(x.shape)
+
+#%% figure
 
 plt.plot(x[:, 0], x[:, 1], ".")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
 
-print(x)
-print(x.shape)
+#%%
+
